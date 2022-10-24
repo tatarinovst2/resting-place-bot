384028d1cab36a89f9059711d6a3a671308691a0
Styagova Maria <90442713+styagovamaria@users.noreply.github.com>
2022-10-25 01:49:48 +0300

    Add functions for visited and favorite places

--- db/db.py
+++ db/db.py
@@ -38,12 +38,29 @@ class Database:
         if not self.select(f'SELECT place_id FROM ratings WHERE place_id = {place_id}'):
             self.execute(f'INSERT INTO ratings (place_id) VALUES ({place_id})')
         self.execute(f'UPDATE ratings SET {col_name} = {col_name} + 1 WHERE place_id = {place_id}')
+    
+     def add_to_visited(self, place_id, user_id):
+        if not self.select(f'SELECT place_id FROM visited WHERE place_id = {place_id} AND user_id = {user_id}'):
+            self.execute(f'INSERT INTO visited (place_id, user_id) VALUES ({place_id}, {user_id})')
+        self.execute(f'UPDATE visited SET is_visited = TRUE WHERE place_id = {place_id} AND user_id = {user_id}')
+   
+    def remove_from_visited(self, place_id, user_id):
+        if not self.select(f'SELECT place_id FROM visited WHERE place_id = {place_id} AND user_id = {user_id}'):
+            self.execute(f'INSERT INTO visited (place_id, user_id) VALUES ({place_id}, {user_id})')
+        self.execute(f'UPDATE visited SET is_visited = FALSE WHERE place_id = {place_id} AND user_id = {user_id}')
+        
+    def add_to_favorite(self, place_id, user_id):
+        if not self.select(f'SELECT place_id FROM favorite WHERE place_id = {place_id} AND user_id = {user_id}'):
+            self.execute(f'INSERT INTO favorite (place_id, user_id) VALUES ({place_id}, {user_id})')
+        self.execute(f'UPDATE favorite SET is_in_favorite = TRUE WHERE place_id = {place_id} AND user_id = {user_id}')
+   
+    def remove_from_favorite(self, place_id, user_id):
+        if not self.select(f'SELECT place_id FROM favorite WHERE place_id = {place_id} AND user_id = {user_id}'):
+            self.execute(f'INSERT INTO favorite (place_id, user_id) VALUES ({place_id}, {user_id})')
+        self.execute(f'UPDATE favorite SET is_in_favorite = FALSE WHERE place_id = {place_id} AND user_id = {user_id}')
 
     def __del__(self):
         if self.connection is not None:
             print('close connection')
             self.connection.close()
             self.connection = None
-
-
-
